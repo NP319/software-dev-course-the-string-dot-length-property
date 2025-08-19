1cf1312fa59a6106c9d3e4437e8804a83f751d73
NikitaDev <nikitapaul229@gmail.com>
2025-08-18 23:15:25 -0500

    stringdotlength

--- string_dot_length.js
+++ string_dot_length.js
@@ -20,11 +20,18 @@ let stringTwo = "JavaScript";
 let stringThree = "";
 
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
+/*let lengthOne;   // Length of stringOne
 let lengthTwo;   // Length of stringTwo
 let lengthThree; // Length of stringThree
+*/
 
 // Your code here
+let lengthOne = stringOne.length;   // Length of stringOne
+let lengthTwo = stringTwo.length;   // Length of stringTwo
+let lengthThree = stringThree.length; // Length of stringThree
 
-
+// For testing
+console.log("Length of stringOne:", lengthOne);
+console.log("Length of stringTwo:", lengthTwo);
+console.log("Length of stringThree:", lengthThree);
 
